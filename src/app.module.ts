ba5fc3658a09f8d43ad0c0a2d60b7830722d96ad
Christian Bustamante <chrisysandra1992@gmail.com>
2020-10-02 09:49:43 -0500

    Fix: Conexion a una base de datos en heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,11 @@ import { UsuarioModule } from './modules/usuario/usuario.module';
   imports: [
     TypeOrmModule.forRoot({
       type: "mysql",
-      host: "localhost",
+      host: "us-cdbr-east-02.cleardb.com",
       port: 3306,
-      username: "root",
-      password: "",
-      database: "nestrestaurant",
+      username: "b227ad44782a5c",
+      password: "17c2bdf6",
+      database: "heroku_0713714f69c97d2",
       synchronize: true,
       entities: ['dist/**/*.entity.js']
     }),
